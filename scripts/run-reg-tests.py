59256db717d7fb4fe5a3f4176af3d76f1599d7bc
Achim Gsell <achim.gsell@psi.ch>
2018-01-25 17:55:50 +0100

    scripts/run-reg-tests.py: run tests in lexical order, reviewed

--- scripts/run-reg-tests.py
+++ scripts/run-reg-tests.py
@@ -25,7 +25,50 @@ totalNrPassed = 0
 opal_args = ""
 
 """
-This method traverses the directory tree. It will check and execute regression tests for the following directory-layouts:
+Scan given directory for regression tests. Regression tests are stored
+in sub-directory of the given directory. The name of the regression test
+specifies the name of the  is th
+
+tests for the following directory-layouts:
+
+    DIR Structure:
+    name/name.in
+         name.rt
+         name.local
+         name.sge
+         reference/name.lbal
+         reference/name.out
+         reference/name.stat
+         reference/name.lbal.md5
+         reference/name.out.md5
+         reference/name.stat.md5
+
+Please make sure you use this naming scheme!
+"""
+def scan_for_tests (dir):
+    rep = Reporter ()
+    os.chdir (dir)
+
+    tests = set ()
+    with os.scandir ('.') as it:
+        for entry in it:
+            if entry.name.startswith('.') or not entry.is_dir():
+                continue
+            # check if all files required are available
+            test = entry.name
+            basename = os.path.join (test, test)
+            if not (os.path.isfile(basename + ".in") and
+                    os.path.isfile(basename + ".rt") and
+                    os.path.isdir(os.path.join (test, "reference"))):
+                continue
+            tests.add (test)
+            rep.appendReport("Found test %s \n" % (test))
+    return tests
+
+                
+"""
+This method traverses the directory tree. It will check and execute regression
+tests for the following directory-layouts:
 
     DIR Structure:
     name/name.in
@@ -42,37 +85,15 @@ This method traverses the directory tree. It will check and execute regression t
 
 Please make sure you use this naming scheme!
 """
-def callback(arg, dirname, fnames):
+
+def run_regression_test (simname, run_local = True, queue_name = ''):
 
     global totalNrPassed
     global totalNrTests
 
-    if "reference" in dirname:
-        return   #exclude svn and reference dirs
-
-    dir = str.split(dirname, "/")
-    simname = dir[len(dir)-1]
-
-    # check if all files required are available
-    if not (os.path.isfile(dirname + "/" + simname + ".in") and \
-       os.path.isfile(dirname + "/" + simname + ".rt") and \
-       os.path.isdir(dirname + "/" + "reference")):
-        return
-
     rep = Reporter()
-    rep.appendReport("Found valid test in %s \n" % dirname)
-
-    # check if we really want to run this test
-    # empty list = run all
-    runtests = arg[0]
-    if runtests:
-        if not simname in runtests:
-            rep.appendReport("User decided to skip regression test %s \n" % simname)
-            rep.appendReport("\n\n")
-            return
-
     d = datetime.date.today()
-    resultdir = "results/" + d.isoformat() + "/" + simname
+    resultdir = os.path.join ("results", d.isoformat(), simname)
     if not os.path.isdir(resultdir):
         os.makedirs (resultdir)
 
@@ -80,17 +101,13 @@ def callback(arg, dirname, fnames):
     simulation_report.addAttribute("name", simname)
     simulation_report.addAttribute("date", "%s" % d)
 
-    rt = RegressionTest(dirname, simname, opal_args, resultdir)
-    rt.run(simulation_report, arg[1], arg[2])
+    rt = RegressionTest (simname, simname, opal_args, resultdir)
+    rt.run (simulation_report, run_local, queue_name)
     totalNrTests += rt.totalNrTests
     totalNrPassed += rt.totalNrPassed
     rep.appendReport("\n\n")
 
-    if runtests:
-        if simname in runtests:
-            runtests.remove(simname)
-            runtests.append("DONE")
-
+    
 def bailout():
     rep = Reporter()
     d = datetime.date.today()
@@ -104,7 +121,8 @@ def addDate(rep):
     date_report = TempXMLElement("Date")
     startDate_report = TempXMLElement("start")
     d = datetime.datetime.today()
-    startDate_report.appendTextNode("%02d-%02d-%02d %02d:%02d:%02d" % (d.year, d.month, d.day, d.hour, d.minute, d.second))
+    now = "%02d-%02d-%02d %02d:%02d:%02d" % (d.year, d.month, d.day, d.hour, d.minute, d.second)
+    startDate_report.appendTextNode (now)
     date_report.appendChild(startDate_report)
     rep.appendChild(date_report)
 
@@ -131,23 +149,66 @@ def addRevisionStrings(rep):
 
     rep.appendChild(revision_report)
 
+"""
+publish results
+"""
+def publish_results (rundir, publish_dir):
+    rep = Reporter ()
+
+    d = datetime.date.today()
+    webfilename = "results_" + d.isoformat() + ".xml"
+    shutil.copy ("results.xml", os.path.join (publish_dir, webfilename))
+
+    # copy plots to publish directory
+    srcdir = os.path.join ("results", d.isoformat(), "plots")
+    dstdir = os.path.join (publish_dir, "plots_" + d.isoformat())
+    subprocess.getoutput("cp -rf " + srcdir + " " + dstdir)
+
+    # copy 'index.html' if it does not exist in the publish directory
+    index_fname = os.path.join (publish_dir, "index.html")
+    if not os.path.exists(index_fname):
+        shutil.copy (os.path.join (rundir, "index.html"), index_fname)
+
+    # update 'index.html'
+    indexhtml = open(index_fname).readlines()
+        
+    # search for the string 'insert here'
+    for line in range(len(indexhtml)):
+        if "insert here" in indexhtml[line]:
+            m = re.search(webfilename, indexhtml[line + 1])
+            fmt="<a href=\"%s\">%s.%s.%s</a> [passed:%d | broken:%d | failed:%d | total:%d] <br/>\n"
+            text = fmt % (webfilename,
+                          d.day, d.month, d.year,
+                          totalNrPassed, rep.NrBroken(), rep.NrFailed(), totalNrTests)
+            
+            if m != None:
+                # result for today already exist, replace it
+                indexhtml[line+1] = text
+            else:
+                # first run
+                indexhtml.insert(line+1, text)
+            break
+    # write new 'index.html' back
+    indexhtmlout = open(index_fname, "w")
+    indexhtmlout.writelines(indexhtml)
+    indexhtmlout.close()
+        
+    # update various files to publish directory
+    shutil.copy (os.path.join (rundir, "ok.png"), publish_dir);
+    shutil.copy (os.path.join (rundir, "nok.png"), publish_dir);
+    shutil.copy (os.path.join (rundir, "results.xslt"), publish_dir)
+    shutil.copy (os.path.join (rundir, "accordion.js"), publish_dir)
+
+    
 def main(argv):
     rep = Reporter()
-    d = datetime.date.today()
-    rep.appendReport("Results: http://amas.web.psi.ch/regressiontests/results_%s_%s_%s.xml \n\n" % (d.day, d.month, d.year))
-
-    # tests are one level up starting from directory of this script
     rundir = sys.path[0]   # get absolute path name of this script
-    regdir = os.path.dirname (rundir)
 
     global totalNrPassed
     global totalNrTests
     global opal_args
     totalNrTests = 0
     totalNrPassed = 0
-    runtests = list()
-    run_with_tests = True
-    run_local = True
 
     parser = argparse.ArgumentParser(description='Run regression tests.')
     parser.add_argument('tests', metavar='tests', type=str, nargs='+',
@@ -170,6 +231,14 @@ def main(argv):
     # get directory where regression tests are installed
     if args.regdir:
         regdir = args.regdir
+        if not os.path.isdir (regdir):
+            rep.appendReport("Error: specified regression test root is not a directory!")
+            bailout()
+            return
+    else:
+        rep.appendReport ("Error: directory with regression tests not specified!")
+        bailout()
+        return
 
     # get directorx where to store results
     if args.regtest_www:
@@ -178,10 +247,9 @@ def main(argv):
         www_folder = os.getenv("REGTEST_WWW")
 
     if (args.publish_results) and (www_folder == None):
-        rep.appendReport("Error: REGTEST_WWW not set")
+        rep.appendReport("Error: publish directory not specified")
         bailout()
         return
-
 
     # get directory whith OPAL binary
     if args.opal_exe_path:
@@ -201,6 +269,19 @@ def main(argv):
 
     # done with the arguments
 
+    ####
+    # scan for valid tests in specified directory and
+    # check whether the requested tests are valid tests
+    
+    regression_tests = scan_for_tests (regdir)
+    for test in runtests:
+        if not test in regression_tests:
+            rep.appendReport("Error: test %s does not exist!" % (test))
+            bailout ()
+            return
+
+    ####
+    # start regression tests
     rep.appendReport("\n")
     rep.appendReport("Start Regression Test on %s \n" % datetime.datetime.today())
     rep.appendReport("==========================================================\n")
@@ -208,74 +289,22 @@ def main(argv):
     addDate(rep)
 
     os.chdir(regdir)
-    #walk the run dir tree
-    arglist = [runtests, True, '']
-    for root, dirs, files in os.walk("./"):
-        callback(arglist, root, files)
 
-    if run_with_tests and len(runtests) > 0:
-        unknownExist = False
-        for test in runtests:
-            if test != "DONE":
-                rep.appendReport(">>> NO TEST CALLED '" + test + "'\n")
-                unknownExist = True
-        if unknownExist:
+    for test in sorted(regression_tests):
+        # run test if runtest is empty or test is in runtests
+        if not runtests or test in runtests:
+            print ("Running test '%s' ..." % (test))
+            run_regression_test (test)
+        else:
+            print ("Skipping test '%s' ..." % (test))
+            rep.appendReport("User decided to skip regression test %s \n" % test)
             rep.appendReport("\n\n")
-
+            
     addRevisionStrings(rep)
-
     rep.dumpXML("results.xml")
 
-    #cp report to webdir and add entry in index.html
     if args.publish_results:
-        failedtests = rep.NrFailed()
-        brokentests = rep.NrBroken()
-        webfilename = "results_" + d.isoformat() + ".xml"
-        shutil.copy ("results.xml", os.path.join (www_folder, webfilename))
-        srcdir = os.path.join ("results", d.isoformat(), "plots")
-        dstdir = os.path.join (www_folder, "plots_" + d.isoformat())
-        subprocess.getoutput("cp -rf " + srcdir + " " + dstdir)
-        index_fname = os.path.join (www_folder, "index.html")
-        if not os.path.exists(index_fname):
-            shutil.copy (os.path.join (rundir, "index.html"), index_fname)
-        ok_sign_fname = os.path.join (www_folder, "ok.png")
-        if not os.path.exists(ok_sign_fname):
-            shutil.copy (os.path.join (rundir, "ok.png"), ok_sign_fname);
-        nok_sign_fname = os.path.join (www_folder, "nok.png")
-        if not os.path.exists(nok_sign_fname):
-            shutil.copy (os.path.join (rundir, "nok.png"), nok_sign_fname);
-
-        indexhtml = open(index_fname).readlines()
-        for line in range(len(indexhtml)):
-            if "insert here" in indexhtml[line]:
-                m = re.search(webfilename, indexhtml[line + 1])
-                fmt="<a href=\"%s\">%s.%s.%s</a> [passed:%d | broken:%d | failed:%d | total:%d] <br/>\n"
-                if m != None:
-                    indexhtml[line+1] = fmt % (
-                                webfilename,
-                                d.day, d.month, d.year,
-                                totalNrPassed, brokentests, failedtests, totalNrTests)
-                else:
-                    indexhtml.insert(line+1, fmt % (
-                                webfilename,
-                                d.day, d.month, d.year,
-                                totalNrPassed, brokentests, failedtests, totalNrTests))
-
-                break
-        indexhtmlout = open(index_fname, "w")
-        indexhtmlout.writelines(indexhtml)
-        indexhtmlout.close()
-        #update xslt formating file
-        shutil.copy (rundir + "/results.xslt", www_folder + "/")
-        shutil.copy (rundir + "/accordion.js", www_folder + "/")
-
-    #move xml results to result-dir
-    if os.path.isfile('results.xml'):
-        resultdir = regdir + "/results/" + d.isoformat()
-        if not os.path.isdir(resultdir):
-            os.mkdir (resultdir)
-
-        subprocess.getoutput("cp -rf " + "results.xml " + resultdir)
+        publish_results (rundir, www_folder)
 
     bailout()
 
